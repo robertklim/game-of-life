d61e7b7dd6089ccd06e67fcfd51162633857a78d
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-07 19:44:25 +0200

    Count cell's neighbors and it's state by their number

--- js/sketch.js
+++ js/sketch.js
@@ -26,6 +26,7 @@ function setup() {
 
 function draw() {
     background(0);
+
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let x = i * resolution;
@@ -37,4 +38,43 @@ function draw() {
             }
         }
     }
+
+    let next = make2DArray(cols, rows);
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let state = grid[i][j];
+
+            // edges
+            if (i == 0 || i == cols - 1 || j == 0 || j == rows -1) {
+                next[i][j] = state;
+            } else {
+                // count live neighbors
+                let sum = 0;
+                let neighbors = countNeighbors(grid, i, j);
+
+                if (state == 0 && neighbors == 3) {
+                    next[i][j] = 1;
+                } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+                    next[i][j] = 0;
+                } else {
+                    next[i][j] = state;
+                }
+            }
+
+        }
+    }
+
+    grid = next;
+}
+
+function countNeighbors(grid, x, y) {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            sum += grid[x + i][y + j];
+        }
+    }
+    sum -= grid[x][y];
+    return sum;
 }
