2953afaed6204337b19a419b1e5821c2b19669e3
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-08 08:25:44 +0200

    Some refactoring

--- js/sketch.js
+++ js/sketch.js
@@ -1,3 +1,8 @@
+let grid;
+let cols;
+let rows;
+let resolution = 10;
+
 function make2DArray(cols, rows) {
     let arr = new Array(cols);
     for (let i = 0; i < arr.length; i++) {
@@ -6,49 +11,53 @@ function make2DArray(cols, rows) {
     return arr;
 }
 
-let grid;
-let cols;
-let rows;
-let resolution = 10;
-
-function setup() {
-    createCanvas(800, 800);
-    cols = width / resolution;
-    rows = height / resolution;
-
-    grid = make2DArray(cols, rows);
+// generate initial state
+function generateSeed(grid, cols, rows) {
     for (let i = 0; i < cols; i++) {
-        for(let j = 0; j < rows; j++) {
-            grid[i][j] = floor(random(2));
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2)); // 1 - living cell; 0 - dead cell
         }
     }
 }
 
-function draw() {
-    background(0);
-
+function drawGrid(grid, cols, rows, resolution) {
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let x = i * resolution;
             let y = j * resolution;
-            if(grid[i][j] == 1) {
+            if (grid[i][j] == 1) {
                 fill(255);
                 stroke(0);
                 rect(x, y, resolution - 1, resolution - 1);
             }
         }
     }
+}
 
+function countNeighbors(grid, x, y) {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            // fix border cells cases
+            let col = (x + i + cols) % cols; // (x=0->x=9; x=9->x=0)
+            let row = (y + j + rows) % rows; // (y=0->y=9; y=9->y=0)
+            // count living cells
+            sum += grid[col][row];
+        }
+    }
+    // exclude cell neighbors are beeing checked
+    sum -= grid[x][y];
+    return sum;
+}
+
+function applyTransition(grid, cols, rows) {
     let next = make2DArray(cols, rows);
-
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let state = grid[i][j];
-
             // count live neighbors
-            let sum = 0;
             let neighbors = countNeighbors(grid, i, j);
-
+            // decide next state
             if (state == 0 && neighbors == 3) {
                 next[i][j] = 1;
             } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
@@ -58,21 +67,22 @@ function draw() {
             }
         }
     }
-
-    grid = next;
+    return next;
 }
 
-function countNeighbors(grid, x, y) {
-    let sum = 0;
-    for (let i = -1; i < 2; i++) {
-        for (let j = -1; j < 2; j++) {
+function setup() {
+    createCanvas(800, 800);
+    cols = width / resolution;
+    rows = height / resolution;
 
-            let col = (x + i + cols) % cols;
-            let row = (y + j + rows) % rows;
+    grid = make2DArray(cols, rows);
+    generateSeed(grid, cols, rows);
+}
 
-            sum += grid[col][row];
-        }
-    }
-    sum -= grid[x][y];
-    return sum;
+function draw() {
+    background(0);
+
+    drawGrid(grid, cols, rows, resolution);
+
+    grid = applyTransition(grid, cols, rows);
 }
